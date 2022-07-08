5827f3cc1dcff548e24cc957f63181efe10f82a6
khyan22 <khyanalvarez@gmail.com>
2022-07-08 00:45:22 -0400

    set up projects folders, db, server and packages

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS tech_o_bell_db;
+
+CREATE DATABASE tech_o_bell_db;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//handlebars
+const exphbs = require('express-handlebars');
+// todo: uncomment |const helpers = require('./utils/helpers');
+const hbs = exphbs.create();
+// todo: add "{ helpers }" to .create()
+//routes
+// todo: uncomment |const routes = require('./controllers');
+const sequelize = require('./config/connection');
+//server call and port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// todo: uncomment |app.use(routes);
+
+//connects to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'))
+});
