b1839c420c57f17591661afbe753a7ad272f9359
khyan22 <khyanalvarez@gmail.com>
2022-07-08 03:28:50 -0400

    added model relations

--- models/index.js
+++ models/index.js
@@ -1,2 +1,41 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+User.belongsToMany(Post, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+User.belongsToMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
