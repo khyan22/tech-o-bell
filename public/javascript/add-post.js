919552d8b4295ccdc6489ef7fdb34aa1d0e75898
khyan22 <khyanalvarez@gmail.com>
2022-07-24 23:32:10 -0400

    added add post functionality

---
+++ public/javascript/add-post.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const post_title = document.querySelector('#post-title').value;
+  const post_body = document.querySelector('#post-body').value;
+  
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      post_title,
+      post_body
+    })
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
